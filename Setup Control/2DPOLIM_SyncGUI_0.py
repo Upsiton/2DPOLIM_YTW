c58954f34049631169acba88860171cd749b708b
Upsiton <ytwangphys@gmail.com>
2022-10-25 01:21:12 +0200

    Graphic User Interface Software for 2DPOLIM

--- Setup Control/2DPOLIM_SyncGUI_0.py
+++ Setup Control/2DPOLIM_SyncGUI_0.py
@@ -7,6 +7,9 @@ Graphic User Interface (GUI) software for system control and image acquisition
 of 2DPOLIM setup. Functionalities are mostly adapted from PyArduino_2DPOLIM_Sync_0.py
 
 Created on Sun Sep 18 15:25:54 2022
+
+Key updates:
+    - First complete design of frame @ 2022/10/24
 """
 # cd D:\\MyDocs\\PROG\\Python\\MyPy
 # cd D:\\MyDocs\\PROG\\Python\\libs\\tkinter\\MariyaSha_Tutorial\\PDFextract_text-main\\finishedProject
@@ -15,6 +18,7 @@ Created on Sun Sep 18 15:25:54 2022
 ###
 
 import tkinter as tk
+from tkinter import ttk # contains Combobox
 from PIL import Image, ImageTk
 
 import time
@@ -30,7 +34,7 @@ root.title("2DPOLIM Acquisition Software")
 
 # set size of window
 canvas = tk.Canvas(root, width=800, height=600)
-canvas.grid(columnspan=5, rowspan=5) #Position a widget in the parent widget in a grid. Splits the canvas into 3 column , where elements can be put in.
+canvas.grid(columnspan=7, rowspan=7) #Position a widget in the parent widget in a grid. Splits the canvas into 3 column , where elements can be put in.
 
 ################################## Template ################################## @ 04/10/2022
 ### Playground of GUI to try it out
@@ -39,16 +43,16 @@ logo = Image.open('logo.png')
 logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo)
 logo_label.image = logo
-logo_label.grid(column=3, row=3)
+logo_label.grid(row=4, column=5)#, sticky="E")
 
 #instructions
-instructions = tk.Label(root, text="2DPOLIM Acquisition Software\n by Yutong Wang") #, font="Raleway")
-instructions.grid(columnspan=3, column=0, row=0) # position of label
+instructions = tk.Label(root, text="2DPOLIM Acquisition Software\n by Yutong Wang", fg="purple", font=("Raleway",15))
+instructions.grid(row=0, column=2) # columnspan=3,)  position of label
 
 # myLabel = tk.Label(root, text="Hello World!")  # does not fully overlap!! Use tk.StringVar() instead
 myLabel0Text = tk.StringVar()
 myLabel0 = tk.Label(root, textvariable = myLabel0Text)
-myLabel0.grid(column=1,row=1)
+myLabel0.grid(row=1,column=2)
 
 # Button
 def myClick():
@@ -65,13 +69,14 @@ def myClick():
 
 myButton = tk.Button(root, text="Click me!", command=myClick, foreground="yellow", background="red")
 # myButton.pack()
-myButton.grid(column=1, row=2)
+# myButton.grid(row=3,column=2)
+myButton.grid(row=0,column=6)
 
-# Print in text box
+# Print in MAIN text box
     # creat text box widget
-main_text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+main_text_box = tk.Text(root, height=20, width=50, padx=5, pady=5)
 main_text_box.insert(1.0, "Show text here: "+'\n') # show text by inserting; The "1.0" here is the position where to insert the text, and can be read as "line 1, character 0". This refers to the first character of the first line
-main_text_box.grid(column=1, row=3)  # position of text box
+main_text_box.grid(row=4,column=2)  # position of text box
 
 # Def a function to print sth in the main_text_box as a duplication of print() in the console
 def TBOX_MN_print(text0='', text1='', text2='', text3=''): # in case of there's more arg. than 1
@@ -94,11 +99,17 @@ def buttonSwitch():
     if buttonSwitch_flag == 0:
         switch_btn_Text.set("Switch ON")
         buttonSwitch_text = "Hello!"
+        # Change the text variable of label widget:
         myLabel0Text.set("Hello!")
+        # Change the color config. of the label widget:
+        myLabel0.config(bg = "yellow", fg = "red")
     else:
         switch_btn_Text.set("Switch OFF")
         buttonSwitch_text = "Bye bye!"
+        # Change the text variable of label widget:
         myLabel0Text.set("Bye bye!")
+        # Change the color config. of the label widget:
+        myLabel0.config(bg = "green", fg = "yellow")
         
     main_text_box.insert('end', buttonSwitch_text + '\n')
     # switch button
@@ -110,10 +121,11 @@ if buttonSwitch_flag == 0:
 else:
     switch_btn_Text.set("Switch OFF")
     
-switch_btn.grid(column=2, row=2)
+# switch_btn.grid(column=2, row=2)
+switch_btn.grid(row=0, column=5)
 
 # Input boxes (fields, Entry) for Changing config.
-entry0 = tk.Entry(root, width=22)
+entry0 = tk.Entry(root, width=15)
 entry0.insert(0, "Enter something:")
 entry0.grid(column=4, row=0)
 
@@ -267,30 +279,102 @@ def closeSerialPort_Laser_Arduino():
         TBOX_MN_print("Error in disconnecting Laser!")
 
 #create action buttons & 
-openSerialPort_Arduino_btn = tk.Button(root, text="Open Serial Port: Arduino", command=lambda:openSerialPort_Arduino(), foreground="yellow", background="green")
-openSerialPort_Laser_btn = tk.Button(root, text="Open Serial Port: Laser", command=lambda:openSerialPort_Laser(), foreground="orange", background="green")
-openSerialPort_Close_btn = tk.Button(root, text="Close ALL Serial Ports", command=lambda:closeSerialPort_Laser_Arduino(), foreground="red", background="yellow")
+openSerialPort_Arduino_btn = tk.Button(root, text="Connect to Arduino", command=lambda:openSerialPort_Arduino(), foreground="white", background="lime green")
+openSerialPort_Laser_btn = tk.Button(root, text="Connect to Laser", command=lambda:openSerialPort_Laser(), foreground="white", background="lime green")
+openSerialPort_Close_btn = tk.Button(root, text="Close ALL Serial Ports", command=lambda:closeSerialPort_Laser_Arduino(), foreground="black", background="white")
 
 #place buttons on grid
-openSerialPort_Arduino_btn.grid(column=2, row=1)
-openSerialPort_Laser_btn.grid(column=3, row=1)
-openSerialPort_Close_btn.grid(column=4, row=1)
+openSerialPort_Arduino_btn.grid(row=1, column=3, pady=10)
+openSerialPort_Laser_btn.grid(row=1, column=4)
+openSerialPort_Close_btn.grid(row=1, column=5)
 
 ## Section B: Laser ON/OFF and configuration of Both Arduino and laser
 ## B1: Laser ON/OFF
 
 # laser ON in CW trigger
-laserON_btn = tk.Button(root, text="Laser ON!", command=lambda:PyArduinoCMD("cwm1"), foreground="yellow", background="red")
+def laserTriggerOn(if_ON):
+    if if_ON == 1:
+        myLabel0Text.set("Laser is ON! (CW mode)")
+        TBOX_MN_print("Laser is ON! (CW mode)")
+        print("CNSL: Laser is ON! (CW mode)")
+        PyArduinoCMD("cwm1")
+    elif if_ON ==0:
+        myLabel0Text.set("Laser is OFF! (CW mode)")
+        TBOX_MN_print("Laser is OFF! (CW mode)")
+        print("CNSL: Laser is OFF! (CW mode)")
+        PyArduinoCMD("cwm0")
+        
+laserON_btn = tk.Button(root, text="Laser ON!", command=lambda:laserTriggerOn(1), foreground="yellow", background="red")
 # PyArduinoCMD("cwm1") #issue: the cam cannot work in cw mode. need to turn off trigger in cam setting manually
-laserON_btn.grid(column=3, row=2)
+laserON_btn.grid(row=3,column=6)
 
 # laser OFF in CW trigger
-laserOFF_btn = tk.Button(root, text="Laser OFF", command=lambda:PyArduinoCMD("cwm0"), foreground="yellow", background="green")
+laserOFF_btn = tk.Button(root, text="Laser OFF", command=lambda:laserTriggerOn(0), foreground="yellow", background="green")
 # PyArduinoCMD("cwm0") #issue: the cam cannot work in cw mode. need to turn off trigger in cam setting manually
-laserOFF_btn.grid(column=4, row=2)
+laserOFF_btn.grid(row=3,column=7)
 
-## B2: Laser config.
+## B2: Laser config. (!!!Funcionality is Unfinished!!!)
+# Read laser power
+laserPowerConfig_Label = tk.Label(root, text="Laser Power (mW): ")
+laserPowerConfig_Label.grid(row=2,column=5, sticky="E")
 
+# Input box
+laserPowerConfig_Entry = tk.Entry(root, width=6)
+laserPowerConfig_Entry.grid(row=2,column=6, sticky="W")
+laserPowerConfig_Entry.insert(0, "click")
+
+pass
+
+# Combobox to select read/set value
+laserPowerConfig_Combobox = ttk.Combobox(root, width = 14) # Combobox is in ttk
+laserPowerConfig_Combobox['value'] = ("Read Laser Power","Set Laser Power") # set manu of combobox
+laserPowerConfig_Combobox.current(0) # set default selection of the first item
+laserPowerConfig_Combobox.grid(row=2,column=7, sticky="W", padx=5)
+
+pass
+
+# Button for executing read/set value
+laserPowerConfig_btn = tk.Button(root, text="Execute",fg="red", bg="light yellow")
+laserPowerConfig_btn.grid(row=2,column=8,sticky="W")
+ 
+pass
+
+
+
+
+## A3: Test LC communication (!!!Funcionality is Unfinished!!!)
+# A changable Label to show status of LC port test
+''' Example:
+myLabel0Text = tk.StringVar()
+myLabel0 = tk.Label(root, textvariable = myLabel0Text)
+
+myLabel0Text.set("Bye bye!")
+# Change the color config. of the label widget:
+myLabel0.config(bg = "green", fg = "yellow")
+'''
+LCPortCkeckLabel0 = tk.Label(root, text = "LC Port Status:")
+LCPortCkeckLabel0.grid(row=3, column=3, sticky="E")
+
+LCPortCkeckText = tk.StringVar()
+LCPortCkeckLabel = tk.Label(root, textvariable = LCPortCkeckText)
+LCPortCkeckLabel.grid(row=3, column=4, sticky="W")
+# Inertial text:
+LCPortCkeckText.set("Undetected")
+LCPortCkeckLabel.config(bg = "yellow", fg = "red")
+
+# Button to check LC communication:
+LCPortCkeck_btn = tk.Button(root, text="Check LC Port", command=lambda:LCPortCkeck(), foreground="white", background="lime green")
+LCPortCkeck_btn.grid(row=3,column=5,sticky="W")
+
+pass
+def LCPortCkeck():
+    if buttonSwitch_flag == 1:
+        LCPortCkeckText.set("Ready")
+        LCPortCkeckLabel.config(bg = "green", fg = "yellow")
+    else:
+        LCPortCkeckText.set("FAILED")
+        LCPortCkeckLabel.config(bg = "yellow", fg = "red")
+    
 
 
 
@@ -346,26 +430,49 @@ def PyArduinoCMD(arduCMD, if_return = False):
 
 # Readout current config. of Arduino. on 2022/10/16-17
 
-# Input fields (Entry) for Changing config.
+triggerConfigLabel = tk.Label(root, text="Trigger Timing\n Configuration:", fg="blue") #, font="Raleway")
+triggerConfigLabel.grid(column=0, row=0, sticky="E")#, columnspan=3) # position of label
+
+# Input fields (Entry) and their labels for Changing config.
+# entry1. # of Full Scan
+entry1_nscan_Label = tk.Label(root, text="# of Full Scan =")
+entry1_nscan_Label.grid(column=0, row=1, sticky="E")
+
 entry1_nscan = tk.Entry(root, width=6)
-entry1_nscan.grid(column=0, row=1)
+entry1_nscan.grid(column=1, row=1, padx=10, sticky="W") # padx: add space between the adjacent widget of xx px.
 entry1_nscan.insert(0, "click 'Show Parameters'")
 # entry1_nscan.delete(0,'end') # or entry1_nscan.delete(0, tk.END) ; tk.END='end'
 
+# entry2. triggerPulseWidthLC 
+entry1_nscan_Label = tk.Label(root, text="triggerPulseWidthLC =")
+entry1_nscan_Label.grid(column=0, row=2, sticky="E")
+
 entry2_lcpt = tk.Entry(root, width=6)
-entry2_lcpt.grid(column=0, row=2)
+entry2_lcpt.grid(column=1, row=2, padx=10, sticky="W")
 entry2_lcpt.insert(0, "click 'Show Parameters'")
 
+# entry3. LCWaitTime = 
+entry3_lcwait_Label = tk.Label(root, text="LCWaitTime =")
+entry3_lcwait_Label.grid(column=0, row=3, sticky="E")
+
 entry3_lcwait = tk.Entry(root, width=6)
-entry3_lcwait.grid(column=0, row=3)
+entry3_lcwait.grid(column=1, row=3, padx=10, sticky="W")
 entry3_lcwait.insert(0, "click 'Show Parameters'")
 
+# entry4. triggerPulseWidthMain =
+entry4_mnpt_Label = tk.Label(root, text="triggerPulseWidthMain =")
+entry4_mnpt_Label.grid(column=0, row=4, sticky="NE")
+
 entry4_mnpt = tk.Entry(root, width=6)
-entry4_mnpt.grid(column=0, row=4)
+entry4_mnpt.grid(column=1, row=4, padx=10, sticky="NW")
 entry4_mnpt.insert(0, "click 'Show Parameters'")
 
+# entry5. triggerDelayTime =
+entry5_pdly_Label = tk.Label(root, text="triggerDelayTime =")
+entry5_pdly_Label.grid(column=0, row=5, sticky="NE")
+
 entry5_pdly = tk.Entry(root, width=6)
-entry5_pdly.grid(column=0, row=5)
+entry5_pdly.grid(column=1, row=5, padx=10, sticky="NW")
 entry5_pdly.insert(0, "click 'Show Parameters'")
 
 # Read and Show current config.
@@ -465,35 +572,69 @@ def setArduinoConfig():
 
 
 ## Implement GUI Widgets 
-showArduinoConfig_btn = tk.Button(root, text="Show Parameters", command=lambda:getArduinoConfig(), foreground="white", background="#20bebe")
-showArduinoConfig_btn.grid(column=2, row=0)
+showArduinoConfig_btn = tk.Button(root, text="Read Trigger Config", command=lambda:getArduinoConfig(), foreground="white", background="#20bebe")
+showArduinoConfig_btn.grid(row=2, column=3, padx=10, pady=10)
 
-setArduinoConfig_btn = tk.Button(root, text="Set Parameters", command=lambda:setArduinoConfig(), foreground="red", background="yellow")
-setArduinoConfig_btn.grid(column=3, row=0)
+setArduinoConfig_btn = tk.Button(root, text="Set Trigger Config", command=lambda:setArduinoConfig(), foreground="red", background="yellow")
+setArduinoConfig_btn.grid(row=2, column=4, padx=10, pady=10)
 
 
 
 ## C2: Operation Modes of Arduino 
 ## Start @ 05/10/2022 0:10
 
-# Buttons
+# 1. Acquisition mode
 def PyArduinoCMD_acqm():
-    myLabel = tk.Label(root, text="Start acquisition!")
+    myLabel0Text.set("Start Acquisition!")
     print("CNSL: PyArduinoCMD_acqm")
-    myLabel.grid(column=1,row=1)
     
     # text_response = "Start acquisition!"
     # main_text_box.insert('end', text_response +'\n') # show text by inserting
+    TBOX_MN_print("Start Acquisition!")
     
     # Action:
     PyArduinoCMD("acqm")
     
     # text_response = "Acquisition is done!"
     # main_text_box.insert('end', text_response +'\n') # show text by inserting
+    TBOX_MN_print("Acquisition is done!")
 
-openSerialPort_Arduino_btn = tk.Button(root, text="Start acquisition!", command=lambda:PyArduinoCMD_acqm(), foreground="yellow", background="red")
-openSerialPort_Arduino_btn.grid(column=2, row=3)
+startAcquisitionMode_btn = tk.Button(root, text="Start Acquisition!", command=lambda:PyArduinoCMD_acqm(), foreground="yellow", background="red")
+startAcquisitionMode_btn.grid(row=4, column=3)
 
+# 2. Infinite triggering mode (!!!Funcionality is Unfinished!!!)
+'''
+PyArduinoCMD("infm")
+
+PyArduinoCMD("infm0")
+'''
+def PyArduinoCMD_infm():
+    myLabel0Text.set("Start Infinite Trigger Mode!")
+    print("CNSL: PyArduinoCMD_infm")
+    
+    TBOX_MN_print("Start Infinite Trigger Mode!")
+    
+    # Action:
+    PyArduinoCMD("infm")
+    
+    TBOX_MN_print("Infinite Trigger Mode is on! Click 'Stop Infinite Trigger Mode' to termninate.")
+
+startInfiniteMode_btn = tk.Button(root, text="Start Infinite\nTrigger Mode", command=lambda:PyArduinoCMD_infm(), fg="red", bg="light yellow")
+startInfiniteMode_btn.grid(row=4, column=4)
+
+def PyArduinoCMD_infm0():
+    myLabel0Text.set("Stop Infinite Trigger Mode!")
+    print("CNSL: PyArduinoCMD_infm0")
+    
+    TBOX_MN_print("Stop Infinite Trigger Mode!")
+    
+    # Action:
+    PyArduinoCMD("infm0")
+    
+    TBOX_MN_print("Infinite Trigger Mode is terminated!")
+
+stopInfiniteMode_btn = tk.Button(root, text="Stop Infinite\nTrigger Mode", command=lambda:PyArduinoCMD_infm0(), fg="light yellow", bg="#20bebe")
+stopInfiniteMode_btn.grid(row=5, column=4, sticky="N")
 
 # end of main window
 root.mainloop()
